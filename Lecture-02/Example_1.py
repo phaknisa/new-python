394dd3069bc010787feb29d37d0a21958bd34012
phaknisa <phaknisa@gmail.com>
2026-07-07 15:37:10 +0700

    Lecture_2

---
+++ Lecture-02/Example_1.py
@@ -0,0 +1,9 @@
+# Getting user input and converting to int
+age = input("Enter your age: ")
+age = int(age)
+
+# Getting user input and converting to float
+height = input("Enter your height: ")
+height = float(height)
+
+print("Your are " + str(age) + " years old and " + str(height) + " feet tall")
---
+++ Lecture-02/TestTypeConv.py
@@ -0,0 +1,13 @@
+age = 25
+height = int(age)
+print(height, age)
+
+height = 5.6
+age = int(height)
+print(age, height)
+
+num_str = "123"
+result = int(num_str) * 2
+print(result)
+num = int(num_str)
+print(num, num_str)
